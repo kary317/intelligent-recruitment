635be1e82669e8be6e063f462fbcae3d2f7b58a9
kary <kary.sun@outlook.com>
2026-05-15 20:07:14 +0800

    实现获取员工列表接口

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
-from typing import Optional
+from typing import Optional, List
 from models.user import UserStatus
 from datetime import datetime
 
@@ -8,12 +8,14 @@ class UserLoginSchema(BaseModel):
     email: EmailStr = Field(..., description="邮箱账号")
     password: str = Field(..., description="密码", min_length=6, max_length=20)
 
+
 class DepartmentSchema(BaseModel):
     id: str = Field(..., description="部门ID")
     name: str = Field(..., description="部门名称")
     description: Optional[str] = Field(..., description="部门描述")
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class UserSchema(BaseModel):
     id: str = Field(..., description="用户ID")
@@ -29,6 +31,7 @@ class UserSchema(BaseModel):
     created_at: datetime = Field(..., description="创建时间")
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class UserLoginRespSchema(BaseModel):
     access_token: str = Field(..., description="access_token")
@@ -47,3 +50,7 @@ class UserRegisterSchema(BaseModel):
     username: str = Field(..., description="用户名")
     realname: str = Field(..., description="真实姓名")
     password: str = Field(..., min_length=6, max_length=20, description="密码")
+
+
+class UserListRespSchema(BaseModel):
+    users: List[UserSchema]
