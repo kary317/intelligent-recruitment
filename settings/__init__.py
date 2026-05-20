410dadc7d88bfd06179dd8e187956c15e50c6172
kary <kary.sun@outlook.com>
2026-05-20 19:48:49 +0800

    添加 测试ocr识别接口

--- settings/__init__.py
+++ settings/__init__.py
@@ -47,6 +47,8 @@ class Settings(BaseSettings):
     # 简历上传存储路径
     RESUME_DIR: str = os.path.join(BASE_DIR, "upload")
 
+    # Paddle OCR Access Token
+    PADDLE_OCR_ACCESS_TOKEN: str = Field(..., validation_alias="PADDLE_OCR_ACCESS_TOKEN")
 
     @computed_field
     @property
